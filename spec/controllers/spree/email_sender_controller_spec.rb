cdf165ef5a05d8faaabad246878d05d310fa2bd1
Tobias Bohwalli <hi@futhr.io>
2013-05-22 02:45:27 +0200

    Updated spec base and add locale spec.
    Add standard dependencies which can be cleaned up later if never used.
    Engine has for the moment no real test coverage.

--- spec/controllers/spree/email_sender_controller_spec.rb
+++ spec/controllers/spree/email_sender_controller_spec.rb
@@ -1,9 +1,7 @@
 require 'spec_helper'
 
 describe Spree::EmailSenderController do
-
   it "should use EmailSenderController" do
     controller.should be_an_instance_of(Spree::EmailSenderController)
   end
-
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,54 @@
-# This file is copied to ~/spec when you run 'ruby script/generate rspec'
-# from the project root directory.
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../dummy/config/environment", __FILE__)
+if ENV["COVERAGE"]
+  require 'simplecov'
+  require 'coveralls'
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+  ]
+  SimpleCov.start do
+    add_filter '/spec/'
+    add_group 'Controllers', 'app/controllers'
+    add_group 'Mailers', 'app/mailers'
+    add_group 'Models', 'app/models'
+    add_group 'Overrides', 'app/overrides'
+    add_group 'Views', 'app/views'
+    add_group 'Libraries', 'lib'
+  end
+end
+
+ENV["RAILS_ENV"] = 'test'
+
+require File.expand_path('../dummy/config/environment.rb',  __FILE__)
 require 'rspec/rails'
+require 'capybara/rspec'
+require 'ffaker'
+require 'database_cleaner'
+
+Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
 
 require 'spree/testing_support/factories'
 require 'spree/testing_support/url_helpers'
 
 RSpec.configure do |config|
-  config.mock_with :rspec
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = false
+  config.include FactoryGirl::Syntax::Methods
   config.include Spree::TestingSupport::UrlHelpers
+  config.include Capybara::DSL, type: :request
+
+  config.color = true
+  config.mock_with :rspec
+  config.use_transactional_fixtures = false
+  config.fail_fast = ENV['FAIL_FAST'] || false
+
+  config.before do
+    if example.metadata[:js]
+      DatabaseCleaner.strategy = :truncation
+    else
+      DatabaseCleaner.strategy = :transaction
+    end
+    DatabaseCleaner.start
+  end
+
+  config.after do
+    DatabaseCleaner.clean
+  end
 end
