9d2e6e59057b806643909bdad65d609a812f95b0
Tobias Bohwalli <hi@futhr.io>
2013-06-27 17:30:06 +0200

    Crate placeholers for specs.
    Yes you can fill out the gap :)

---
+++ spec/controllers/spree/admin/captcha_settings_controller_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe Spree::Admin::CaptchaSettingsController do
+  stub_authorization!
+
+  context "#update" do
+  end
+end
--- spec/controllers/spree/email_sender_controller_spec.rb
+++ spec/controllers/spree/email_sender_controller_spec.rb
@@ -1,7 +1,12 @@
 require 'spec_helper'
 
 describe Spree::EmailSenderController do
-  it "should use EmailSenderController" do
-    controller.should be_an_instance_of(Spree::EmailSenderController)
+  context "controller instance" do
+    it "use Spree::EmailSenderController" do
+      controller.should be_an_instance_of Spree::EmailSenderController
+    end
+  end
+
+  context "#send_mail" do
   end
 end
