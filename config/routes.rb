269e6f3c1878997fd6575fad2113e1defb875151
Tobias Bohwalli <hi@futhr.io>
2013-07-20 04:30:25 +0200

    Addpend routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Spree::Core::Engine.routes.prepend do
+Spree::Core::Engine.routes.append do
   match '/email_to_friend/:type/:id' => 'email_sender#send_mail', as: :email_to_friend
   namespace :admin do
     resource :captcha_settings
